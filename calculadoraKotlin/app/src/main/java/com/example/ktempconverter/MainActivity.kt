c2ce105e04538aca401ccd5f5aba5478fc1e784e
aramsesporcayomercado <94558953+aramsesporcayomercado@users.noreply.github.com>
2022-09-27 23:09:20 -0500

    Add files via upload

---
+++ calculadoraKotlin/app/src/main/java/com/example/ktempconverter/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.ktempconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var celciusRadio: RadioButton
+    private lateinit var fahreinheitRadio: RadioButton
+    lateinit var converterButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.valorTemp) as EditText
+
+        celciusRadio = findViewById(R.id.celciusRadio) as RadioButton
+        fahreinheitRadio = findViewById(R.id.fahreinheitRadio) as RadioButton
+        kelvinRadio = findViewById(R.id.celciusRadio) as RadioButton
+        rankieRadio = findViewById(R.id.fahreinheitRadio) as RadioButton
+
+
+        converterButton = findViewById(R.id.converterButton) as Button
+        converterButton.setOnClickListener { converter(it) }
+
+    }
+
+    fun converter(view: View) {
+        var temp: Double = editText.text.toString().toDouble()
+
+        if (celciusRadio.isChecked) {
+            temp = (temp - 32) * 5/9
+        } else if (fahreinheitRadio.isChecked) {
+            temp = temp * 9/5 + 32
+        }if(kelvinRadio.isChecked){
+            temp=temp + 273.15
+        }else if(rankieRadio.isChecked){
+            temp = (temp+273.15)*9/5
+
+        }
+
+
+
+
+        editText.setText(temp.toString())
+    }
+}
